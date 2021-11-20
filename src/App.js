2be246c84ef572abc296aaea08e33a12ffb13d72
Siva Rajan <citysiva180@gmail.com>
2021-11-20 23:10:09 +0530

    completed rendering tables

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StateCheck from "./components/LearningState/LearningState";
 import ToggleEvents from "./components/ReactEvents/RaisingEvents";
+import RenderingTables from "./components/RenderingTables/RenderingTables";
+
 function App() {
   return (
     <div className="container">
@@ -10,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<StateCheck />}></Route>
           <Route path="/events" element={<ToggleEvents />}></Route>
+          <Route path="/tables" element={<RenderingTables />}></Route>
         </Routes>
       </Router>
     </div>
--- src/components/RenderingTables/RenderingTables.js
+++ src/components/RenderingTables/RenderingTables.js
@@ -1,1 +1,50 @@
 import React, { Component } from "react";
+import CountriesData from "../../countries.json";
+import { Table } from "react-bootstrap";
+
+class RenderingTables extends Component {
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {};
+  //   }
+  render() {
+    return (
+      <div className="container">
+        <h1>Rendering a JSON Data into a Table in React</h1>
+        <br />
+        <br />
+        <br />
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Country Name</th>
+              <th>Capital</th>
+              <th>Airport Code</th>
+              <th>Currency</th>
+              <th>Region</th>
+              <th>Emoji</th>
+            </tr>
+          </thead>
+          <tbody>
+            {CountriesData.map((countryData, index) => {
+              return (
+                <tr key={countryData.id}>
+                  <td>{countryData.id}</td>
+                  <td>{countryData.name}</td>
+                  <td>{countryData.capital}</td>
+                  <td>{countryData.iso2}</td>
+                  <td>{countryData.currency}</td>
+                  <td>{countryData.region}</td>
+                  <td>{countryData.emoji}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+}
+
+export default RenderingTables;
